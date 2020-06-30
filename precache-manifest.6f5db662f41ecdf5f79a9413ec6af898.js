cc68457c70cf9198d519a563c8159f44be2a7cb4
balwantsinghmnit <balwantsinghmnit@gmail.com>
2020-06-30 11:34:01 +0530

    Updates

---
+++ precache-manifest.6f5db662f41ecdf5f79a9413ec6af898.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ef016e4fbbc4632b69e8492a4a3252f",
+    "url": "/React-Redux-JSONHolder-API/index.html"
+  },
+  {
+    "revision": "c93019fecb852dfe3103",
+    "url": "/React-Redux-JSONHolder-API/static/css/main.d3bb61ce.chunk.css"
+  },
+  {
+    "revision": "b648399eb7cfc3356293",
+    "url": "/React-Redux-JSONHolder-API/static/js/2.db02d6cf.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-Redux-JSONHolder-API/static/js/2.db02d6cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c93019fecb852dfe3103",
+    "url": "/React-Redux-JSONHolder-API/static/js/main.ad05e5ca.chunk.js"
+  },
+  {
+    "revision": "d913fb6dc6aea629f2d2",
+    "url": "/React-Redux-JSONHolder-API/static/js/runtime-main.ff212097.js"
+  }
+]);
